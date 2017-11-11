88277957bf4089ad2e82683a01bfd297cd914f15
Rafael Cruz <rafaelcruz1@hotmail.com>
2017-11-11 11:07:36 -0200

    feat: dividir

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -20,7 +20,17 @@ var Calculadora = {
         return num1 - num2;
     },
     dividir: function(num1, num2){
-        return num1 - num2;
+
+        if(num2 == 0){
+            return "Erro";
+        }
+
+        if(isNaN(num1) || isNaN(num2)){
+            return 0;
+        }
+
+        return num1 / num2;
+
     },
     multiplicar: function(){
         return 0;
