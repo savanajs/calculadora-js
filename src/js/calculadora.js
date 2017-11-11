25605b08a6394f59d2d0fef6ac09aaf17644f1d6
Rafael Cruz <rafaelcruz1@hotmail.com>
2017-11-11 13:03:43 -0200

    feat: multiplicacao

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,23 +1,19 @@
 var Calculadora = {
+
+    ADICAO: '+',
+    SUBTRACAO: '-',
+    DIVISAO: '/',
+    MULTIPLICACAO: '*',
+
     adicionar: function(num1, num2){
 
-        if(isNaN(num1) || isNaN(num2)){
-            return 0;
-        }
-
-        num1 = parseFloat(num1);
-        num2 = parseFloat(num2);
-
-        return num1 + num2;
+        return this.calcular(num1, num2, this.ADICAO);
 
     },
     subtrair: function(num1, num2){
 
-        if(isNaN(num1) || isNaN(num2)){
-            return 0;
-        }
+        return this.calcular(num1, num2, this.SUBTRACAO);
 
-        return num1 - num2;
     },
     dividir: function(num1, num2){
 
@@ -25,20 +21,46 @@ var Calculadora = {
             return "Erro";
         }
 
-        if(isNaN(num1) || isNaN(num2)){
-            return 0;
-        }
-
-        return num1 / num2;
+        return this.calcular(num1, num2, this.DIVISAO);
 
     },
     multiplicar: function(num1, num2){
 
+        return this.calcular(num1, num2, this.MULTIPLICACAO);
+
+    },
+    // Refactory
+    calcular(num1, num2, operacao){
+
+        var resultado = 0;
+
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+
         if(isNaN(num1) || isNaN(num2)){
             return 0;
         }
 
-        return num1 * num2;
+        switch (operacao) {
+            case Calculadora.ADICAO:
+                 resultado = num1 + num2;
+                 break;
+            case Calculadora.SUBTRACAO:
+                 resultado = num1 - num2;
+                 break;
+            case Calculadora.DIVISAO:
+                 resultado = num1 / num2;
+                 break;
+            case Calculadora.MULTIPLICACAO:
+                 resultado = num1 * num2;
+                 break;
+            default: 
+                 resultado = 0;
+                 break;
+        }
+
+        return resultado;
+
     }
 }
 
